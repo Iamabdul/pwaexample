59f63d1630d4807d1fee79a09f2d437b8ca2a8d8
Abdul Al-haj <a.alhaj@hotmail.co.uk>
2021-05-30 14:49:56 +0100

    pwa init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import FetchWeather, { fetchWeather } from './api/fetchWeather';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState({});
+
+    const search = async (e) => {
+        if(e.key === 'Enter'){
+            const data = await fetchWeather(query);
+            setWeather(data); // the setWeather is sort of assigning the weather variable in line 7 to the data coming in.
+            setQuery(''); // similarly here but it's a string. The query and weather variables are accessible globally. This action is to just reste the query.
+            // we needed to do this since data in the search function is local in this scope only.
+        }
+    }
+    return (
+        <div className="main-container">
+            <input
+                type="text"
+                className="search"
+                placeholder="search a place!"
+                value = {query}
+                onChange = {(event) => setQuery(event.target.value)}
+                onKeyPress = { search }
+            />
+
+            {weather.main && ( //this is checking if the weather.main exists, then it goes to the next segmetn of the condition (which in our case seems to be an action)
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                    </div>
+                </div>
+            )}
+
+        </div>
+    )
 }
 
-export default App;
+export default App; // this is so we can use this App outside and let other files import it
